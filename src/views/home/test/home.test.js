e199c3be3746e5f0a25f8ab291d73aed553539bb
Mauricio Paz Melgar <mauriciopazmelgar@CBB-LP413.local>
2022-07-26 12:54:55 -0400

    Complete code challenge with test

--- src/views/home/test/home.test.js
+++ src/views/home/test/home.test.js
@@ -1,23 +1,124 @@
-import { screen, renderHook } from "@testing-library/react";
+import { screen, renderHook, render, act } from "@testing-library/react";
 import Home from "../../../views/home/home/index";
-import { renderWithState } from "../../../utils/utilsTest/utilsTest";
 import { useHome } from "../home/useHome";
+import { Provider } from "react-redux";
+import configureStore from "redux-mock-store";
+import {
+  fetchDataGoogle,
+  fetchDataBing,
+  fetchAllData,
+} from "../../../store/actions/homeActions";
+import thunk from "redux-thunk";
+import axios from "axios";
+const middlewares = [thunk];
+const initialState = {
+  home: {
+    data: null,
+    loading: false,
+    success: false,
+    error: false,
+  },
+};
 
-const mockDispatch = jest.fn();
-jest.mock("react-redux", () => ({
-  useSelector: jest.fn(),
-  useDispatch: () => mockDispatch,
-}));
+jest.mock("axios");
 
-describe("Test Home", () => {
-  it("render learn react home page", () => {
-    renderWithState(<Home />);
+const mockStore = configureStore(middlewares);
+
+describe("With React Testing Library", () => {
+  it("Render Component Home Page", () => {
+    const store = mockStore(initialState);
+    render(
+      <Provider store={store}>
+        <Home />
+      </Provider>
+    );
     const HomePage = screen.getByText(/Home Page/i);
     expect(HomePage).toBeInTheDocument();
   });
+});
 
-  it("useHome Hook", () => {
-    const { result } = renderHook(() => useHome());
-    console.log(result);
+describe("useHome Hook", () => {
+  const store = mockStore(initialState);
+  const wrapper = ({ children }) => (
+    <Provider store={store}>{children}</Provider>
+  );
+
+  it("should call function handleInputText", () => {
+    const { result } = renderHook(() => useHome(), { wrapper });
+    act(() => {
+      result.current.handleInputText("test");
+    });
+    expect(result.current.inputText).toBe("test");
+  });
+
+  it("should call function handleSelectValue", () => {
+    const { result } = renderHook(() => useHome(), { wrapper });
+    act(() => {
+      result.current.handleInputText(1);
+    });
+    expect(result.current.selectValue).toBe(1);
+  });
+
+  it("should call function handleSubmitData for Google", async () => {
+    const { result } = renderHook(() => useHome(), { wrapper });
+    axios.get.mockResolvedValueOnce({ data: {} });
+    const fetchData = store.dispatch(
+      fetchDataGoogle({
+        term: result.current.inputText,
+        value: 1,
+      })
+    );
+    act(() => {
+      result.current.handleSubmitData();
+    });
+
+    expect(fetchData).toEqual(
+      Promise.resolve({
+        data: {},
+      })
+    );
+  });
+
+  it("should call function handleSubmitData for bing", async () => {
+    const { result } = renderHook(() => useHome(), { wrapper });
+    axios.get.mockResolvedValueOnce({ data: {} });
+
+    const fetchData = store.dispatch(
+      fetchDataBing({
+        term: result.current.inputText,
+        value: 2,
+      })
+    );
+
+    act(() => {
+      result.current.handleSubmitData();
+    });
+
+    expect(fetchData).toEqual(
+      Promise.resolve({
+        data: {},
+      })
+    );
+  });
+
+  it("should call function handleSubmitData for All", async () => {
+    const { result } = renderHook(() => useHome(), { wrapper });
+
+    axios.get.mockResolvedValueOnce({ data: {} });
+    const fetchData = store.dispatch(
+      fetchAllData({
+        term: result.current.inputText,
+        value: 3,
+      })
+    );
+    act(() => {
+      result.current.handleSubmitData();
+    });
+
+    expect(fetchData).toEqual(
+      Promise.resolve({
+        data: {},
+      })
+    );
   });
 });
